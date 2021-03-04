a51ae6cee01699491a3c14942fa937214081bbd7
thunsirikorn <thunsirikorn.n@gmail.com>
2021-03-04 16:10:50 +0700

    angular3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BackendService } from './backend.service' ;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private backendService: BackendService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,78 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillComponent } from './skill/skill.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BackendService } from './backend.service';
+import { HomeComponent } from './home/home.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+    },
+  {
+  path: 'home',
+  component: HomeComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+  path: 'profile',
+  component: ProfileComponent
+  },
+  {
+  path: 'education',
+  component: EducationComponent
+  },
+  {
+  path: 'experience',
+  component: ExperienceComponent
+  },
+  {
+    path: 'skill',
+    component: SkillComponent
+  },
+  {
+  path: '**',
+  component: PageNotFoundComponent
+  }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainContentComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProfileComponent,
+    SkillComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [BackendService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
